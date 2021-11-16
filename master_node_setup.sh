a71bae9bbd2a0b070e38186ab78f08e39c5bb304
Jaibw <jai.unix@gmail.com>
2021-11-16 11:11:45 +0530

    Create master_node_setup.sh

---
+++ master_node_setup.sh
@@ -0,0 +1,5 @@
+kubeadm init --apiserver-advertise-address=[your-eth0-ip-of-master-node] --pod-network-cidr=192.168.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
